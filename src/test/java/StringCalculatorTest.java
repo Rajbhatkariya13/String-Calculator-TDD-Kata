e0d4ee57608aad7b0620d7e8e7845f5ad0d04a8b
Rajbhatkariya13 <raj.bhadkariya1344@gmail.com>
2021-04-27 19:34:30 +0530

    3-Handled new lines between numbers in Test Class

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,5 +10,43 @@ public class StringCalculatorTest
 {
         stringCalculator = new StringCalculator();
 }
+@Test
+    public void getZeroForEmptyString(){
+        Assert.assertEquals(0, stringCalculator.add(""));
+    }
+    @Test
+    public void sumOfOneInteger(){
+        Assert.assertEquals(1, stringCalculator.add("1"));
+    }
+    @Test
+    public void sumOfTwoInteger(){
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class StringCalculatorTest {
+    StringCalculator stringCalculator;
+    @Before
+    public void init(){
+        stringCalculator = new StringCalculator();
+    }
+ @Test
+    public void getZeroForEmptyString(){
+        Assert.assertEquals(0, stringCalculator.add(""));
+    }
+    @Test
+    public void sumOfOneInteger(){
+        Assert.assertEquals(1, stringCalculator.add("1"));
+    }
+    @Test
+    public void sumOfTwoInteger(){
+        Assert.assertEquals(3, stringCalculator.add("1,2"));
+    }
 
+@Test
+    public void addNumbersWithNewLine(){
+        Assert.assertEquals(6, stringCalculator.add("1\n2,3"));
+    }
+
+}
+}
 }
