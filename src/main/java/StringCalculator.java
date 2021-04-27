73b28a9b33a835a7061e1f6bd9454449f9045fd2
Rajbhatkariya13 <raj.bhadkariya1344@gmail.com>
2021-04-27 19:19:21 +0530

    3-Handled new lines between numbers in Main Class

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,17 +1,14 @@
 public class StringCalculator 
 {
-  public int add(String numbers)
+public int add(String numbers)
 {
-if (numbers.equals(""))
-            return 0;
-
-        String[] sNumbers = numbers.split(",");
-        int count = 0;
-        for(String v : sNumbers)
-       {
-            count += Integer.parseInt(v.trim());
-        }
-        return count;
-
-    }
+if(numbers.equals("")) return 0;
+String[] sNumbers = numbers.split("[,\n]");
+int count = 0;
+for(String v : sNumbers)
+{
+count += Integer.parseInt(v.trim());
+ }
+return count;
+}
 }
