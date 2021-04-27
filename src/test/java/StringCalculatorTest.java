34a6973cd46943141a9d09c501135ccf29406ea1
Rajbhatkariya13 <raj.bhadkariya1344@gmail.com>
2021-04-27 18:43:16 +0530

    Test Class Creation

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringCalculatorTest 
+{
+    StringCalculator stringCalculator;
+    @Before
+    public void init()
+{
+        stringCalculator = new StringCalculator();
+}
+
+}
