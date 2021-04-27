5eb9432ed5a3c109d101e6139b005737514e78a0
Rajbhatkariya13 <raj.bhadkariya1344@gmail.com>
2021-04-27 20:21:21 +0530

    7-GetCalledCount returning addCount in Main Class

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,10 +1,12 @@
 public class StringCalculator 
 {
+int addCalledCount=0;
 public int add(String numbers)
 {
+addCalledCount++;
 if(numbers.equals("")) return 0;
 String customDelimiter;
-if (numbers.contains("//")) 
+if(numbers.contains("//")) 
 {
 int newLineIndex = numbers.indexOf('\n');
 customDelimiter = numbers.substring(2, newLineIndex);
@@ -18,9 +20,14 @@ for(String v : sNumbers)
 {
 count += Integer.parseInt(v.trim());
 int number = Integer.parseInt(v.trim());
-if (number < 0) negativeNumbers.add(v.trim());
+if(number < 0) negativeNumbers.add(v.trim());
 count += number;
 }
+if(negativeNumbers.size() > 0) throw new IllegalArgumentException("Negatives not allowed" + (negativeNumbers.size() > 1 ? (" " + String.join(",", negativeNumbers)) : ""));
 return count;
+}
+public int getCalledCount()
+{
+return addCalledCount;
 }
 }
