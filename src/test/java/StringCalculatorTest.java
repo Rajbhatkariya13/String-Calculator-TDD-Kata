cb29a35c04c29d94c701d4b0ca6e75255d4a263d
Rajbhatkariya13 <raj.bhadkariya1344@gmail.com>
2021-04-27 19:48:28 +0530

    4-Support for different delimiters in Test Class

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -46,6 +46,10 @@ public class StringCalculatorTest {
     public void addNumbersWithNewLine(){
         Assert.assertEquals(6, stringCalculator.add("1\n2,3"));
     }
+@Test
+    public void supportDifferentDelimiter() {
+        Assert.assertEquals(4, stringCalculator.add("//;\n1;3"));
+    }
 
 }
 }
