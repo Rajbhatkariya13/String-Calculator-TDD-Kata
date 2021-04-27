6f5e0b0d5274ad9563a8414494426a670102799a
Rajbhatkariya13 <raj.bhadkariya1344@gmail.com>
2021-04-27 19:00:29 +0530

    1 & 2 - adding empty to unknown number of integers separated by commas in Test Class

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,6 +1,29 @@
-public class StringCalculator 
-{
-    public int add(String numbers){
-        return 0;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+    StringCalculator stringCalculator;
+    @Before
+    public void init(){
+        stringCalculator = new StringCalculator();
     }
+
+ @Test
+    public void getZeroForEmptyString(){
+        Assert.assertEquals(0, stringCalculator.add(""));
+    }
+
+    @Test
+    public void sumOfOneInteger(){
+        Assert.assertEquals(1, stringCalculator.add("1"));
+    }
+
+    @Test
+    public void sumOfTwoInteger(){
+        Assert.assertEquals(3, stringCalculator.add("1,2"));
+    }
+
+}
+}
 } 
